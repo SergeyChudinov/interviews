952c50f0c1f0c8260e9afe969aa17f93dc2d08ba
Sergey Chudinov <sergeychudinov1986@gmail.com>
2023-03-22 10:29:01 +0500

    Add 4 interviews

---
+++ Павел Тарасов Ютюб/01-Мария - первое в жизни frontend-собеседование/index.js
@@ -0,0 +1,68 @@
+// d = {
+//     '+': (a, b) => a + b,
+//     '-': (a, b) => a - b,
+//     '/': (a, b) => a / b,
+//     '>=': (a, b) => a >= b
+// }
+// function exe(a, op, b) {
+//     return d[op](a, b);
+// }
+
+// console.log(exe(6, '-', 3));
+// console.log(exe(6, '+', 3));
+// console.log(exe(6, '/', 3));
+// console.log(exe(6, '>=', 3));
+
+// function execute(a, b, c) {
+// 	// return eval(`a ${b} c`);
+// 	return new Function('x', 'y', `return x ${b} y`)(a, c);
+// }
+// console.log(execute(6, '-', 3));
+// console.log(execute(6, '>=', 3));
+
+// const arr = [2,3,1,4,21]
+// const filterArr = arr.filter(n => n >= 2) 
+// console.log(filterArr)
+// const sortArr = arr.sort((a, b) => a - b)
+// console.log(sortArr)
+
+// const array = [-3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+// function isPrime(num) {
+// 	for (let i = 2; num > i; i++) {
+// 		if (num % i == 0) {
+// 			return false;
+// 		}
+// 	}
+// 	return num > 1;
+// }
+// console.log(array.filter(isPrime)); // [2, 3, 5, 7, 11, 13]
+
+// function filter(arr, func) {
+// 	return arr.filter(func);
+// }
+// const res = filter([1, 3, 2, 4, 7, 0], n => n < 3);
+// console.log(res);
+
+function filter(arr, func, thisArg) {
+	let filterArr = [];
+	for (let i = 0; i < arr.length; i++) {
+		typeof arr[i] === 'number' && func.call(thisArg, arr[i], i, arr) && filterArr.push(arr[i]);
+	}
+	return filterArr;
+}
+// const res = filter([1, 3, 2, 4, 7, 0, '5'], (n, i , arr) => n < 3);
+const res = filter([1, 3, 2, 4, 7, 0, '5'], function (val, i, arr) {
+	return val < this.val;
+}, {val: 5});
+console.log(res);
+
+// const a = "30";
+// const b = "20";
+// const operator = "+";
+// const formula = a + operator + b;
+// console.log(eval(formula));
+
+// const i = Infinity;
+// if (i + 1 === i) {
+// 	console.log('Infinity')
+// }
---
+++ Павел Тарасов Ютюб/02-Собеседование и волнение начинающего frontend-разработчика/index.js
@@ -0,0 +1,72 @@
+function getStringIndexes(str) { //Мое!
+	// const filterStr = str.replace(/([^a-zа-яё])/ig, '').toLowerCase(); // ['a', 'a', 'a', 'a', 'a', 'b', 'b', 'b', 'b', 'b', 'c', 'c', 'c', 'c', 'c', 'a']
+	const array = str.toLowerCase().split(''); //['a', 'a', 'a', 'a', 'a', 'b', 'b', 'b', 'b', 'b', '_', '+', '=', '*', 'c', 'c', 'c', 'c', 'c', 'a']
+	const lettersArray = []; //  ['a', 'b', 'c']
+	for (const val of array) { // 1 filterStr | array
+		if (!lettersArray.includes(val) && val.match(/([a-zа-яё])/i)) {
+			lettersArray.push(val)
+		}
+	}
+	const stringArray = [];
+	for (const val of lettersArray) { //val = a,b,c
+		const indexArray = []; // 0,1,2,3,4,15     5,6,7,8,9     14,15,16,17,18
+		for (let i = 0; i < array.length; i++) { // 2 filterStr | array
+			if (array[i] == val) { // 3 filterStr | array
+				indexArray.push(i)
+			}
+
+		}
+		stringArray.push(`${val}: [${indexArray}]`);
+	}
+	return stringArray;
+}
+console.log(getStringIndexes('aaaaAbbbbBccccCA')); //['a: [0,1,2,3,4,15]', 'b: [5,6,7,8,9]', ...]
+console.log(getStringIndexes('aaaaAbbbbB_+=*ccccCA')); //['a: [0,1,2,3,4,19]', 'b: [5,6,7,8,9]', ...]
+
+function getStringIndexes(str) { //Ученика!
+	const filterStr = str.replace(/[^a-zA-Z]/g, '').toLowerCase(); //'aaaaAbbbbBccccCA'
+	const obj = {}; //{a: [0, 1, 2, 3, 4, 15], b: [5, 6, 7, 8, 9], c: [10, 11, 12, 13, 14]}
+	for (let i = 0; i < filterStr.length; i++) {
+		if (obj[filterStr[i]]) {
+			obj[filterStr[i]].push(i)
+		} else {
+			obj[filterStr[i]] = [i];
+		}
+	}
+	const stringArray = [];
+
+	// for (const val in obj) { // a, b, c
+	// 	stringArray.push(`${val}: [${obj[val]}]`);
+	// }
+
+	Object.keys(obj).forEach(el => {
+		stringArray.push(`${el}: [${obj[el]}]`)
+	})
+	return stringArray
+}
+// console.log(getStringIndexes('aaaaAbbbbBccccCA')); //['a: [0,1,2,3,4,15]', 'b: [5,6,7,8,9]', 'c: [10,11,12,13,14]']
+console.log(getStringIndexes('aaaaAbbbbB_+=*ccccCA')); //['a: [0,1,2,3,4,15]', 'b: [5,6,7,8,9]', 'c: [10,11,12,13,14]']
+
+function getStringIndexes(str) { //В 1 строку!
+	const stringArray = []
+	const result = Object.entries(str.toLowerCase().replace(/[^a-z]/g, '').split('').reduce((res, el, i) => {
+		res[el] = res[el] ? [...res[el], i] : [i]
+		return res
+	},{}));
+	console.log(result); //[['a', Array(6)], ['b', Array(5)], ['c', Array(5)]]
+	(function() {
+		result.forEach(el => {
+		 	stringArray.push(`${el[0]}: [${el[1]}]`)
+		})
+	}())
+	return stringArray
+}
+console.log(getStringIndexes('aaaaAbbbbBccccCA'));
+
+function getStringIndexes(str) { //Еще короче!
+	return Object.entries(str.toLowerCase().replace(/[^a-z]/g, '').split('').reduce((res, el, i) => {
+		res[el] = res[el] ? [...res[el], i] : [i]
+		return res
+	}, {})).map(el => `${el[0]}: [${el[1]}]`);
+}
+console.log(getStringIndexes('aaaaAbbbbB-ccccCAt')); 
